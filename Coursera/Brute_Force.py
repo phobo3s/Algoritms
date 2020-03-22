8b3794b5066b0e6fd03386968e4882a36ccb926e
phobo3s <bberkkilinc@gmail.com>
2020-03-22 20:08:33 +0300

    2

--- Coursera/Brute_Force.py
+++ Coursera/Brute_Force.py
@@ -16,12 +16,46 @@ def FindWay(G,i,j):
     perms = CreatePermutations(perms, betweenValues, maxPermLen)
     print(perms)
 
+perms = []
+tempList = []
+
+def addperm(values,tempList):
+    a = 0
+    for i in tempList:
+        for q in range(len(values)):
+            j = values[a]
+            a += 1
+            i.append(j)
+
+def perm2(values,leng):
+    for i in values:
+        for j in range(len(values)):
+            tempList.append([i])
+    for i in range(leng-1):
+        addperm(values,tempList)
+
+    return tempList
+
+perm2([1,2,3,4],2)
+print(tempList)
+
+
+
+
+
+
+
+
 def CreatePermutations(perms, values, maxLen):
-    if len(values) == maxLen:
-        for i in values:
-            perms.append(i)
-    maxLen -= 1
+    perms = []
+    tempList = []
+    for i in values:
+        perms.append(i)
+    for i in values:
+        for j in values:
+            if i != j:
+                tempList.append(i)
+                perms.append(tempList)
+
     return perms
 
-
-FindWay(G,1,3)
