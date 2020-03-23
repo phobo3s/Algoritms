e32c30723443e72f4bb81d3a4e86002717a2aa7f
phobo3s <bberkkilinc@gmail.com>
2020-03-23 15:28:21 +0300

    last commit

--- Fibionachi/Fibionachi.py
+++ Fibionachi/Fibionachi.py
@@ -33,18 +33,6 @@ def Fibio(n):
         fib = Fibio(n-1) + Fibio(n-2)
     return fib
 
-def Comparison(n):
-    b= n
-    epltime = time.time()
-    fib = Fibio(n)
-    epltime = time.time() - epltime
-    print("The {}. fibionacci number is {}. It took {} seconds to calculate that without Memoization".format(n,fib,epltime))
-
-    epltime = time.time()
-    fib = FibioMemor(b)
-    epltime = time.time() - epltime
-    print("The {}. fibionacci number is {}. It took {} seconds to calculate that with Memoization".format(b,fib,epltime))
-
 def FibioSeq(FibList, n):
     if n == 1:
         FibList = [0]
@@ -59,8 +47,19 @@ def FibioSeqMemor(FibList, n):
         FibList.append(FibioSeqMemor(FibList,n-1))
     return FibioMemor(n)
 
+
+def Comparison(n):
+    epltime = time.time()
+    fib = Fibio(n)
+    epltime = time.time() - epltime
+    print("The {}. fibionacci number is {}. It took {} seconds to calculate that without Memoization".format(n,fib,epltime))
+
+    epltime = time.time()
+    fib = FibioMemor(n)
+    epltime = time.time() - epltime
+    print("The {}. fibionacci number is {}. It took {} seconds to calculate that with Memoization".format(n,fib,epltime))
+
 def ComparisonSeq(n):
-    a = n
     epltime = time.time()
     FibList = []
     FibioSeq(FibList,n+1)
@@ -69,9 +68,10 @@ def ComparisonSeq(n):
 
     epltime = time.time()
     FibList = []
-    FibioSeqMemor(FibList,a+1)
+    FibioSeqMemor(FibList,n+1)
     epltime = time.time() - epltime
     print("The Sequence is - {}.\n it took {} second with memoization.".format(FibList,epltime))
+
 
 # Test Functions
 def MegaTest(a):
