f866b7142d1560a2a2e52cc4318ca614161e7ab6
phobo3s <bberkkilinc@gmail.com>
2020-03-25 20:34:43 +0300

    Done

--- Coursera/Brute_Force.py
+++ Coursera/Brute_Force.py
@@ -2,11 +2,55 @@ import os
 import time
 
 # Graph Matrix
-G = [[0,1,0,1,0],[0,0,1,0,0],[0,0,0,1,1],[0,0,0,0,0]]
+#G = [[0,1,0,1,0],[0,0,1,0,0],[0,0,0,1,1],[0,0,0,0,0]]
+
+def ReadGraphFromFile(name):
+    workingDir = os.getcwd() + "\\"   
+    try:
+        size = 0
+        file = open(workingDir + name, "r") 
+        connections = file.readlines()
+        for i in range(len(connections)):
+            connections[i] = connections[i].replace("\n","").split(" ")
+            connections[i][0] = int(connections[i][0])
+            connections[i][1] = int(connections[i][1])
+            if size < connections[i][0]:
+                size = connections[i][0]
+            elif size < connections[i][1]:
+                size = connections[i][1]
+            else:
+                pass
+        file.close()
+        G = []
+        # Make Zero Array
+        for x in range(size+1):
+            G.append([])
+            for y in range(size+1):
+                G[x].append([])
+                G[x][y] = 0
+        for i in connections:
+            G[i[0]][i[1]] = 1
+        input("Yükleme Tamamlandı 'enter'a Basınız...\n")
+        return G
+    except FileExistsError:
+        print("Graph Dosyası Bulunamadı ... ")
+        input("Geri Dönmek İçin 'enter'a Basınız...\n")
+
+def WriteGraphToFile(G,name):
+    workingDir = os.getcwd() + "\\"   
+    file = open(workingDir + name, "w")
+    
+    file.write()
+
+
+
+
+    file.close()
 
 def PrintGraph(G):
     for i in G:
         print(i)
+    input("Geri Dönmek İçin 'enter'a Basınız...\n")
     
 def FindWay(G,i,j):  
     betweenValues = [x for x in range(len(G)) if x != i and x != j]
@@ -70,9 +114,11 @@ def Permutate(values,leng):
 
 def Menu():
     print(50*"=")
-    print("[1] - Bir noktadan diğerine yol bul...")
-    print("[2] - Bir noktanın derecesini bul...")
-    print("[3] - Küçük Dünya mı?")
+    print("[1] - Dosyadan Graph Bilgilerini Al")
+    print("[2] - 1")
+    print("[3] - Bir noktadan diğerine yol bul...")
+    print("[4] - Bir noktanın derecesini bul...")
+    print("[5] - Küçük Dünya mı?")
     print("")
     print("[0] - Çıkış...")
     selection = input("Lütfen Seçiminizi Yapınız... \n")
@@ -83,11 +129,15 @@ def Main():
         os.system("cls")
         selection = Menu()
         if selection == "1":
+            G = ReadGraphFromFile("gr1.txt")
+            PrintGraph(G)
+        elif selection == "3":
             startNode = int(input("Başlangıç Noktasını Giriniz...\n"))
             endNode = int(input("Bitiş Noktasını Giriniz...\n"))
+            #G = ReadGraphFromFile("gr1.txt")
             FindWay(G,startNode,endNode)
             input("Geri Dönmek İçin 'enter'a Basınız...\n")
-        elif selection == "2":
+        elif selection == "4":
             pass
         elif selection == "0":
             print("Hoşçakal")
@@ -97,10 +147,9 @@ def Main():
         else:
             print("Hatalı seçim yaptınız lütfen tekrar deneyiniz...")
 
-        
-
 if __name__ == "__main__":
     Main()
+    pass
 
 
 #DEPRECATED
